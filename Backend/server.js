ea9e5676b21cfad0b6679395ad8c3dc815ce623e
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-01 23:47:01 -0700

    created server

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Create an instance of Express
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB Configuration
+const dbURI = 'mongodb://localhost:27017/';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Smart Budget API');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
