5912be1354aa66b7a04df27f658bb72eb70387a8
Khoa Le <khoale@Khoas-MacBook-Pro.local>
2023-07-02 14:56:47 -0700

    added login and register endpoints to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Create an instance of Express
 const app = express();
@@ -10,19 +10,57 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// User data (for simplicity, keeping it in-memory)
+let users = [];
+
 // MongoDB Configuration
-const dbURI = 'mongodb://localhost:27017/';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const dbURI = "mongodb://localhost:27017/";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
   });
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to the Smart Budget API');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Smart Budget API");
+});
+
+// Register endpoint
+app.post("/register", (req, res) => {
+  const { username, password, first_name, last_name, email } = req.body;
+
+  // Check if the username is already taken
+  if (users.find((user) => user.username === username)) {
+    return res.status(400).json({ error: "Username already exists" });
+  }
+
+  // Create a new user object
+  const newUser = { username, password, first_name, last_name, email };
+
+  // Save the user in the user list
+  users.push(newUser);
+  console.log(users);
+
+  return res.status(201).json({ message: "Registration successful" });
+});
+
+// Login endpoint
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Find the user with the provided username
+  const user = users.find((user) => user.username === username);
+
+  // Check if the user exists and the password matches
+  if (!user || user.password !== password) {
+    return res.status(401).json({ error: "Invalid username or password" });
+  }
+
+  return res.status(200).json({ message: "Login successful" });
 });
 
 // Start the server
