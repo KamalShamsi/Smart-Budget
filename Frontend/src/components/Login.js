34af62864c7414d9f25273cb863487aec2ebf520
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-03 21:43:31 -0700

    Login page and other components of Frontend

---
+++ Frontend/src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // Login successful
+        console.log('Login successful');
+      } else {
+        // Login failed
+        const errorData = await response.json();
+        console.error('Login failed:', errorData.error);
+      }
+    } catch (error) {
+      console.error('An error occurred while logging in:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
