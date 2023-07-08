01fff46f679b0e6bb249c4b9628311f611baccae
bmquach <bmquach@ucsc.edu>
2023-07-08 00:04:37 -0700

    pretty

--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import LoginForm from '../../components/LoginForm';
+import styles from './style.css'
+import Card from '../../components/Card'
 
 const LoginPage = () => {
   return (
-    <div>
-      <h2>Login</h2>
-      <LoginForm />
+    <div className='background'>
+      <div className='login_card_container'>
+        <Card color='white' width='40%'>
+          <h2>Login</h2>
+          <LoginForm />
+        </Card>
+      </div>
     </div>
   );
+
+
+
 };
 
 export default LoginPage;
