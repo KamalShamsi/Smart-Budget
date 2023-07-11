a0ea96be576d0993f50a6aa6f5f4fcc7b7f5d071
Khoa Le <khoale@Khoas-MacBook-Pro.local>
2023-07-10 22:02:39 -0700

    fixed login page responsiveness

--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import LoginForm from '../../components/LoginForm';
-import styles from './style.css'
-import Card from '../../components/Card'
+import React from "react";
+import LoginForm from "../../components/LoginForm";
+import "./styles.css";
+import Card from "../../components/Card";
 
 const LoginPage = () => {
   return (
-    <div className='background'>
-      <div className='login_card_container'>
-        <Card color='white' width='40%'>
+    <div className="login_page_wrapper">
+      <div className="login_card_container">
+        <Card color="white">
           <h2>Login</h2>
           <LoginForm />
         </Card>
       </div>
     </div>
   );
-
-
-
 };
 
 export default LoginPage;
