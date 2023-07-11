087f54b26f44f083f14d5abb6ab7c17d7aa50bc6
Khoa Le <khoale@Khoas-MacBook-Pro.local>
2023-07-10 21:45:43 -0700

    modified profile page

--- Frontend/src/components/Card.js
+++ Frontend/src/components/Card.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Card = ({ color, children, height, width }) => {
+const Card = ({ color, children, height, width, marginBottom }) => {
   const cardStyle = {
     backgroundColor: color,
-    borderRadius: '2rem',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    padding: '2rem',
-    marginBottom: '1rem',
-    height: height || 'auto',
-    alignItems: 'center',
-    width: width || 'auto',
+    borderRadius: "2rem",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    padding: "2rem",
+    marginBottom: marginBottom || "1rem",
+    height: height || "auto",
+    alignItems: "center",
+    width: width || "auto",
   };
 
   return <div style={cardStyle}>{children}</div>;
--- Frontend/src/pages/Profile/index.js
+++ Frontend/src/pages/Profile/index.js
@@ -1,35 +1,19 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import BottomBar from '../../components/BottomBar';
+import React from "react";
+import BottomBar from "../../components/BottomBar";
+import ProfileCards from "../../components/ProfileCards";
 
 export default function Profile() {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    navigate('/login'); // Redirect to the login page after logout
-  };
-
   return (
     <div>
-      <h3>Profile</h3>
-      <form>
-        <label>
-          Username:
-          <input type="text" value="ExampleUsername" readOnly />
-        </label>
-        <br />
-        <label>
-          First Name:
-          <input type="text" value="ExampleFirstName" readOnly />
-        </label>
-        <br />
-        <label>
-          Last Name:
-          <input type="text" value="ExampleLastName" readOnly />
-        </label>
-      </form>
-      <button onClick={handleLogout}>Logout</button> {/* Add the logout button */}
-      <BottomBar />
+      <div className="dashboard-header">
+        <h3 style={{ margin: 0 }}>Profile</h3>
+      </div>
+      <hr />
+      <ProfileCards />
+
+      <div className="bottomBar">
+        <BottomBar />
+      </div>
     </div>
   );
 }
