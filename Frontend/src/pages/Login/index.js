95092b947982daaaefb742fe9e5f18de3189b889
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-13 21:56:42 -0700

    Different style login page

--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import LoginForm from "../../components/LoginForm";
-import "./styles.css";
 import Card from "../../components/Card";
+import './styles.css';
 
 const LoginPage = () => {
   return (
     <div className="login_page_wrapper">
       <div className="login_card_container">
+        <div className="background_container">
+          <h1 className="background_text">Smart Budget</h1>
+          <p className="background_text_secondary">
+            Smart Decisions start here
+          </p>
+        </div>
         <Card color="white">
-          <h2>Login</h2>
+          <h2 className="login_title">Login</h2>
           <LoginForm />
         </Card>
       </div>
