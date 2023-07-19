0a7dbb71ef81ee5e1dcd277336e1c580e3e59891
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-19 10:19:33 -0700

    Profile user personal info window new style

--- Frontend/src/pages/Profile/index.js
+++ Frontend/src/pages/Profile/index.js
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Grid, Paper, Button, Link, TextField } from '@mui/material';
+import { Box, Typography, Grid, Paper, Button, Link, Avatar } from '@mui/material';
 import {
   Home as HomeIcon,
   AddCircle as AddCircleIcon,
   AccountCircle as AccountCircleIcon,
   MonetizationOn as MonetizationOnIcon,
   ExitToApp as ExitToAppIcon,
-  GetApp as GetAppIcon,
 } from '@mui/icons-material';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
 const Profile = () => {
   const [userProfile, setUserProfile] = useState('');
-  const [editMode, setEditMode] = useState(false);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
 
   const getUserProfile = async () => {
     try {
@@ -37,51 +33,13 @@ const Profile = () => {
     getUserProfile();
   }, []);
 
-  const handleEditClick = () => {
-    setEditMode(true);
-  };
-
-  const handleSaveClick = async () => {
-    try {
-      const token = Cookies.get('tok');
-      const response = await axios.post(
-        'http://localhost:8000/profile',
-        {
-          firstName,
-          lastName,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-      if (response.status === 200) {
-        setUserProfile(response.data.profile);
-        setFirstName(response.data.profile.first_name);
-        setLastName(response.data.profile.last_name);
-        setEditMode(false);
-      }
-    } catch (error) {
-      console.error('Failed to update user profile:', error);
-      setEditMode(false);
-    }
-  };
-
   return (
     <Box bgcolor="#0d47a1" minHeight="100vh" p={3}>
       <Box textAlign="center" mb={3}>
         <Typography variant="h4" color="white">
           Profile
         </Typography>
-        <Box
-          bgcolor="#1565c0"
-          height={2}
-          width={150}
-          mx="auto"
-          my={2}
-          borderRadius={5}
-        />
+        <Box bgcolor="#1565c0" height={2} width={150} mx="auto" my={2} borderRadius={5} />
       </Box>
       <Grid container spacing={3} justifyContent="center">
         <Grid item xs={6} sm={3} md={2}>
@@ -176,32 +134,19 @@ const Profile = () => {
             </Link>
           </Paper>
         </Grid>
-        {/* Profile-specific components */}
-        <Grid item xs={12} md={10}>
-          <Paper elevation={3} sx={{ p: 2, height: '100%' }}>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              height="100%"
-            >
-              <Box textAlign="center" color="black">
-                <Typography variant="body1" mb={1}>
-                  Username: {userProfile.username}
-                </Typography>
-                <Typography variant="body1" mb={1}>
-                  First Name: {userProfile.first_name}
-                </Typography>
-                <Typography variant="body1" mb={1}>
-                  Last Name: {userProfile.last_name}
-                </Typography>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={10}>
+        <Grid item xs={12} md={6} lg={5}>
           <Paper elevation={3} sx={{ p: 2 }}>
-            <Box textAlign="center">
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <AccountCircleIcon fontSize="large" color="primary" sx={{ mb: 2 }} />
+              <Typography variant="h6" color="primary" sx={{ mb: 1 }}>
+                {userProfile.username}
+              </Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                First Name: {userProfile.first_name}
+              </Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                Last Name: {userProfile.last_name}
+              </Typography>
               <Button
                 variant="contained"
                 startIcon={<ExitToAppIcon />}
