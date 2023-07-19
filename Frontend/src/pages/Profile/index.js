c196b0bdc429d334ada3217a98fa505a3aab2357
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-18 23:06:42 -0700

    Profile page link for money management fixed

--- Frontend/src/pages/Profile/index.js
+++ Frontend/src/pages/Profile/index.js
@@ -124,7 +124,7 @@ const Profile = () => {
               >
                 <AddCircleIcon fontSize="large" color="white" />
                 <Typography variant="body1" color="white" mt={1}>
-                  Add
+                  Money Management
                 </Typography>
               </Box>
             </Link>
