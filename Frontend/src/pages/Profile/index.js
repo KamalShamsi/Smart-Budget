6e89abc68288ab88ddc6bc849f819f5b556062e7
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-18 23:03:11 -0700

    Edit button is removed in profile page

--- Frontend/src/pages/Profile/index.js
+++ Frontend/src/pages/Profile/index.js
@@ -8,20 +8,19 @@ import {
   ExitToApp as ExitToAppIcon,
   GetApp as GetAppIcon,
 } from '@mui/icons-material';
-import Cookies from "js-cookie";
-import axios from "axios";
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 const Profile = () => {
-
-  const [userProfile, setUserProfile] = useState("");
+  const [userProfile, setUserProfile] = useState('');
   const [editMode, setEditMode] = useState(false);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
   const getUserProfile = async () => {
     try {
-      const token = Cookies.get("tok");
-      const response = await axios.get("http://localhost:8000/profile", {
+      const token = Cookies.get('tok');
+      const response = await axios.get('http://localhost:8000/profile', {
         headers: {
           Authorization: token,
         },
@@ -30,7 +29,7 @@ const Profile = () => {
         setUserProfile(response.data.profile);
       }
     } catch (error) {
-      console.error("Failed to fetch user profile:", error);
+      console.error('Failed to fetch user profile:', error);
     }
   };
 
@@ -44,9 +43,9 @@ const Profile = () => {
 
   const handleSaveClick = async () => {
     try {
-      const token = Cookies.get("tok");
+      const token = Cookies.get('tok');
       const response = await axios.post(
-        "http://localhost:8000/profile",
+        'http://localhost:8000/profile',
         {
           firstName,
           lastName,
@@ -64,7 +63,7 @@ const Profile = () => {
         setEditMode(false);
       }
     } catch (error) {
-      console.error("Failed to update user profile:", error);
+      console.error('Failed to update user profile:', error);
       setEditMode(false);
     }
   };
@@ -186,74 +185,16 @@ const Profile = () => {
               justifyContent="center"
               height="100%"
             >
-              <Box
-                display="flex"
-                alignItems="center"
-                mb={2}
-                sx={{ color: 'white' }}
-              >
-                <Typography variant="h6" ml={1} color="black">
-                  {userProfile.username}
-                </Typography>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={10}>
-          <Paper elevation={3} sx={{ p: 2, height: '100%' }}>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              height="100%"
-            >
               <Box textAlign="center" color="black">
-              {editMode ? (
-                  <>
-                    <TextField
-                      label="First Name"
-                      value={firstName}
-                      onChange={(e) => setFirstName(e.target.value)}
-                      fullWidth
-                      mb={1}
-                    />
-                    <TextField
-                      label="Last Name"
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
-                      fullWidth
-                      mb={1}
-                    />
-                    <Button
-                      variant="contained"
-                      onClick={handleSaveClick}
-                      sx={{ mt: 2, width: '100%' }}
-                      startIcon={<GetAppIcon />}
-                    >
-                      Save
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Typography variant="body1" mb={1} color="black">
-                      First Name: {userProfile.first_name}
-                    </Typography>
-                    <Typography variant="body1" mb={1} color="black">
-                      Last Name: {userProfile.last_name}
-                    </Typography>
-                    <Typography variant="body1" mb={1} color="black">
-                      Email: 
-                    </Typography>
-                    <Button
-                      variant="contained"
-                      onClick={handleEditClick}
-                      sx={{ mt: 2, width: '100%' }}
-                      startIcon={<ExitToAppIcon />}
-                    >
-                      Edit
-                    </Button>
-                  </>
-                )}
+                <Typography variant="body1" mb={1}>
+                  Username: {userProfile.username}
+                </Typography>
+                <Typography variant="body1" mb={1}>
+                  First Name: {userProfile.first_name}
+                </Typography>
+                <Typography variant="body1" mb={1}>
+                  Last Name: {userProfile.last_name}
+                </Typography>
               </Box>
             </Box>
           </Paper>
