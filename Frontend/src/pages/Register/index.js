ddb3165f582e85cd33c803b3fe559bf27f1bc087
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-21 09:40:23 -0700

    Footer is added to Login and Register pages

--- Frontend/src/pages/Register/index.js
+++ Frontend/src/pages/Register/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import RegisterForm from "../../components/RegisterForm";
+import Footer from "../../components/Footer";
 
 const RegisterPage = () => {
   return (
     <div>
       <RegisterForm />
+      <Footer/>
     </div>
   );
 };
