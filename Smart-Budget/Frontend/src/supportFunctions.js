7e5a71e68ca46cedfe6f7440a1ab4071e047cab7
Tipty <103543257+Tipty@users.noreply.github.com>
2023-07-24 17:20:03 -0700

    dupe

--- Smart-Budget/Frontend/src/supportFunctions.js
+++
@@ -1,4 +0,0 @@
-export const formatNumber = (numberString) => {
-  const number = parseFloat(numberString.replace(/,/g, '')); // Remove existing commas, if any
-  return number.toLocaleString();
-};
