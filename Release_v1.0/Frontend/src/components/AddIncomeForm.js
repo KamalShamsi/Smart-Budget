ecc086276c4257673a1ed5e6b31d29f2e1106e08
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-27 09:26:57 -0700

    AddIncomeForm comments

--- Release_v1.0/Frontend/src/components/AddIncomeForm.js
+++ Release_v1.0/Frontend/src/components/AddIncomeForm.js
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
 
 const AddIncomeForm = ({ onAddIncome }) => {
+  // State variables to store the input values for income name, value, and category
   const [incomeName, setIncomeName] = useState('');
   const [incomeValue, setIncomeValue] = useState('');
   const [incomeCategory, setIncomeCategory] = useState('');
 
+  // Event handler for income name input change
   const handleIncomeNameChange = (e) => {
     setIncomeName(e.target.value);
   };
 
+  // Event handler for income value input change
   const handleIncomeValueChange = (e) => {
     setIncomeValue(e.target.value);
   };
 
+  // Event handler for income category selection change
   const handleIncomeCategoryChange = (e) => {
     setIncomeCategory(e.target.value);
   };
 
+  // Event handler for form submission
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
+    // Create a new income object with the input values
     const newIncome = {
       name: incomeName,
       value: incomeValue,
       category: incomeCategory,
     };
 
+    // Call the onAddIncome function with the new income object to add it to the incomes list
     onAddIncome(newIncome);
+
+    // Clear the input fields after adding the income
     setIncomeName('');
     setIncomeValue('');
     setIncomeCategory('');
@@ -44,11 +53,14 @@ const AddIncomeForm = ({ onAddIncome }) => {
         <input type="text" value={incomeValue} onChange={handleIncomeValueChange} />
       </label>
       <label>
-        Expense Category:
+        Income Category:
+        {/* Dropdown select for income category */}
         <select value={incomeCategory} onChange={handleIncomeCategoryChange}>
           <option value="">Select Category</option>
+          {/* Map through the incomeCategories array to generate options for each category */}
           {incomeCategories.map((category, index) => (
             <option key={index} value={category.name}>
+              {/* Display category name along with its icon */}
               {category.icon} {category.name}
             </option>
           ))}
@@ -56,6 +68,7 @@ const AddIncomeForm = ({ onAddIncome }) => {
       </label>
       <br />
       <br />
+      {/* Submit button to add the income */}
       <button type="submit">Add Income</button>
     </form>
   );
