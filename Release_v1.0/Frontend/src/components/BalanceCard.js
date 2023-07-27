f59bd1a35b14071ccbe5302bf1a1f276f001950f
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-27 09:29:54 -0700

    BalanceCard comments

--- Release_v1.0/Frontend/src/components/BalanceCard.js
+++ Release_v1.0/Frontend/src/components/BalanceCard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Card from './Card';
 import { formatNumber } from '../supportFunctions';
+
+// BalanceCard component that displays the available balance.
+// It receives the 'balance' prop, which is the current available balance to be shown.
 const BalanceCard = ({ balance }) => {
   return (
+    // Custom Card component with a blue background color and a height of 10rem.
     <Card color='#064495' height={'10rem'}>
       <div
         style={{
@@ -13,10 +17,12 @@ const BalanceCard = ({ balance }) => {
           height: '100%',
         }}
       >
+        {/* Section displaying the title "Available Balance" */}
         <div>
           <p style={{ color: 'white', marginBottom: '8px' }}>
             Available Balance
           </p>
+          {/* Section displaying the formatted available balance amount */}
           <p
             style={{
               fontSize: '24px',
@@ -28,6 +34,7 @@ const BalanceCard = ({ balance }) => {
             ${formatNumber(balance) || 0}
           </p>
         </div>
+        {/* Link to view more details, directing to '/details' */}
         <a
           style={{
             textDecoration: 'none',
