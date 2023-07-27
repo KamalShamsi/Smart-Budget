2ed3516000c6e304af78d87b3d5796c43dd0756e
Kamal Shamsi <kamalshamsi@kadmins-MBP.attlocal.net>
2023-07-27 09:36:37 -0700

    MonthlyBudgetCard comments

--- Release_v1.0/Frontend/src/components/MonthlyBudgetCard.js
+++ Release_v1.0/Frontend/src/components/MonthlyBudgetCard.js
@@ -18,11 +18,15 @@ const Month = {
   December: 'December',
 };
 
+// MonthlyBudgetCard component
 const MonthlyBudgetCard = ({ balance, month }) => {
   return (
+    // Card component to display the monthly budget
     <Card color='white'>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {/* Display the month and year for the budget */}
         <span style={{ fontWeight: '600' }}>Budget for {Month[month]}:</span>
+        {/* Display the budget amount formatted with commas */}
         <span style={{ fontWeight: '600' }}>${formatNumber(balance) || 0}</span>
       </div>
     </Card>
